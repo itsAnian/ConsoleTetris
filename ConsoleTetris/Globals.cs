a2dcb46d9aad34362e682555846e389a66134316
anian <anian.seidl@gmail.com>
2024-10-06 13:18:29 +0200

    Add Comment

--- ConsoleTetris/Globals.cs
+++ ConsoleTetris/Globals.cs
@@ -2,6 +2,8 @@ namespace ConsoleTetris;
 
 public static class Globals
 {
+    //Standard: WIDTH = 10
+    //Standard: HIGHT = 20
     public const int WIDTH = 10;
     public const int HIGHT = 20;
 }
